4bc0bf1b6cd1bc58fd769692d58f3f0efa8fdf80
Alex322322 <alex9999091@gmail.com>
2025-11-11 18:37:26 +0300

    add path param in handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,11 @@ func main() {
 	// server
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/{city}", func(w http.ResponseWriter, r *http.Request) {
+		city := chi.URLParam(r, "city")
+
+		fmt.Printf("requested city: %s\n", city)
+
 		_, err := w.Write([]byte("welcome 1231231"))
 		if err != nil {
 			log.Println(err)
